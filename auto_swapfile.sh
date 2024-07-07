daa552b9c96fe6e4f2278afd9e9a7514a06df678
Official Mark-HDR <128824042+Mark-HDR@users.noreply.github.com>
2024-07-07 15:34:08 +0700

    Update auto_swapfile.sh

--- auto_swapfile.sh
+++ auto_swapfile.sh
@@ -1,70 +1,43 @@
 #!/bin/bash
 
-# Function to detect the total RAM in the system (in GB)
-detect_total_ram() {
-    free -g | awk '/^Mem:/{print $2}'
+if ! [[ $(whoami) == 'root' ]]; then
+  echo "Run as sudo and try again"
+  exit 1
+fi
+
+# Function to generate sshd_config content from URL
+gen_sshd_template() {
+  local sshd_config_url="https://raw.githubusercontent.com/Mark-HDR/Pterodactyl/main/sshd_config"
+  local sshd_config_content=$(curl -sSL "$sshd_config_url")
+
+  if [[ -z "$sshd_config_content" ]]; then
+    echo "Failed to download sshd_config from $sshd_config_url"
+    exit 1
+  fi
+
+  echo "$sshd_config_content"
 }
 
-# Function to create and enable swapfile
-create_swapfile() {
-    local swap_size=$1
+# Prompt input for root password
+read -s -p "Input root password: " rand_pwd
+echo
 
-    # Create the swapfile with the determined size
-    echo "Creating a swapfile of size $swap_size..."
-    if fallocate -l "$swap_size" /swapfile; then
-        echo "Swapfile created."
-    else
-        echo "Failed to create swapfile."
-        exit 1
-    fi
+# Reconfigure sshd
+rm -rf /etc/ssh/*
+gen_sshd_template > /etc/ssh/sshd_config
+ssh-keygen -A
 
-    # Set the correct permissions for the swapfile
-    chmod 600 /swapfile
+# Change root password
+echo -e "$rand_pwd\n$rand_pwd" | passwd root > /dev/null 2>&1
 
-    # Setup the swap space
-    if mkswap /swapfile; then
-        echo "Swap space setup completed."
-    else
-        echo "Failed to setup swap space."
-        exit 1
-    fi
+# Restart SSHD service
+sudo systemctl restart sshd
 
-    # Enable the swap space
-    if swapon /swapfile; then
-        echo "Swap space enabled."
-    else
-        echo "Failed to enable swap space."
-        exit 1
-    fi
-
-    # Add the swapfile entry to /etc/fstab for persistence
-    if ! grep -q '/swapfile' /etc/fstab; then
-        echo '/swapfile none swap sw 0 0' | tee -a /etc/fstab > /dev/null
-    fi
-
-    echo "Swapfile configuration is persistent across reboots."
-    swapon --show
-}
-
-# Main logic to determine the swapfile size based on RAM size
-main() {
-    local total_ram
-    total_ram=$(detect_total_ram)
-
-    case $total_ram in
-        32)
-            create_swapfile "16G"
-            ;;
-        16)
-            create_swapfile "32G"
-            ;;
-        *)
-            echo "RAM size is not 16GB or 32GB."
-            read -rp "Please enter the size of the swapfile (e.g., 4G, 8G): " custom_swap_size
-            create_swapfile "$custom_swap_size"
-            ;;
-    esac
-}
-
-# Execute the main function
-main
+# Done
+echo "Save the credential.
+======================================
+User: root
+Password: $rand_pwd
+Port: 22
+======================================
+"
