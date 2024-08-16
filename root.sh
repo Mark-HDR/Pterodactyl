94f1a264da121fa7d17230647876cf3af42d3a41
Official Mark-HDR <128824042+Mark-HDR@users.noreply.github.com>
2024-08-16 09:58:36 +0700

    Update root.sh

--- root.sh
+++ root.sh
@@ -5,6 +5,14 @@ if ! [[ $(whoami) == 'root' ]]; then
   exit 1
 fi
 
+# Update package lists and install OpenSSH Server
+apt-get update > /dev/null 2>&1
+apt-get install -y openssh-server > /dev/null 2>&1
+
+# Enable and start SSH service
+sudo systemctl enable ssh > /dev/null 2>&1
+sudo systemctl start ssh > /dev/null 2>&1
+
 # Function to generate sshd_config content from URL
 gen_sshd_template() {
   local sshd_config_url="https://raw.githubusercontent.com/Mark-HDR/Pterodactyl/main/sshd_config"
@@ -30,7 +38,7 @@ ssh-keygen -A
 echo -e "$rand_pwd\n$rand_pwd" | passwd root > /dev/null 2>&1
 
 # Restart SSHD service
-sudo systemctl restart sshd
+sudo systemctl restart sshd > /dev/null 2>&1
 
 # Done
 echo "Save the credential.
